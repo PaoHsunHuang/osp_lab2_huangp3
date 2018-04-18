2352106978d4d26aac2200f38ed178eb61ab20c1
Huang <huangp3@debian-9.3.0-template>
2018-04-18 15:48:25 -0700

    Finish new_cat with full test, file and without parameter

--- new_cat.c
+++ new_cat.c
@@ -1,18 +1,21 @@
 #define _GUN_SOURCE
 #include<stdio.h>
 #include<stdlib.h>
-
 int main(int argc, char *argv[]){
+	//if there is more than one parameter, assume it as
+	//the input file name
+	if(argc > 1) {
 	char *inputText = NULL;
 	FILE *inputFile;
 	size_t lineSize= 256;
 
-	if(argc > 1) {
 	inputFile = fopen(argv[1],"r");
-
+	//if can open file run though whole file and print it
+	//or print out warning if can't find file
 		if(inputFile != NULL){
 
-		while(getline(&inputText,&lineSize,inputFile) != -1){
+		while(getline(&inputText,&lineSize,
+			inputFile) != -1){
 		printf("%s", inputText);
 		}
 		}else{
@@ -20,6 +23,20 @@ int main(int argc, char *argv[]){
 		}
 	fclose(inputFile);
 	}
+	else{
+	char ch = 0;
+	char *inputString = NULL;
+	int x = 0;
+	while(ch != EOF)
+	{
+	ch = getchar();
+	if(ch != EOF){
+	printf("%c", ch);
+	}
+	}
+
+
+
 
 	//int i = argc;
 	//printf("input: %d data\n",argc);
@@ -27,7 +44,7 @@ int main(int argc, char *argv[]){
 	//for(int k = 0; k < argc; ++k){
 	//printf("data %d = %s\n",k ,argv[k]);
 	//}
-
+	}
 	return 0;
 }
 
