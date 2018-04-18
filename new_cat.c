ddf999e48ff6735f432dcb90cb45a80544da92ff
Huang <huangp3@debian-9.3.0-template>
2018-04-17 21:05:07 -0700

    Finish read file and print out for new_cat

--- new_cat.c
+++ new_cat.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+#define _GUN_SOURCE
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
+int main(int argc, char *argv[]){
+	char *inputText = NULL;
+	FILE *inputFile;
+	size_t lineSize= 256;
 
+	if(argc > 1) {
+	inputFile = fopen(argv[1],"r");
 
-return 0;
+		if(inputFile != NULL){
+
+		while(getline(&inputText,&lineSize,inputFile) != -1){
+		printf("%s", inputText);
+		}
+		}else{
+		printf("Can't not find direction");
+		}
+	fclose(inputFile);
+	}
+
+	//int i = argc;
+	//printf("input: %d data\n",argc);
+
+	//for(int k = 0; k < argc; ++k){
+	//printf("data %d = %s\n",k ,argv[k]);
+	//}
+
+	return 0;
 }
+
